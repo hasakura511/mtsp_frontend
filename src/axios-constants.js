a3bafcd18122bbc35ec07ec3274b13ad8e24fe5b
Richard <yuukifushimi@gmail.com>
2018-05-22 08:44:59 +0800

    p

--- src/axios-constants.js
+++ src/axios-constants.js
@@ -3,5 +3,7 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL: 'https://gsm-constants.firebaseio.com',
-  CancelToken: axios.CancelToken
+  CancelToken: axios.CancelToken,
+  timeout: 600000,
+
 });
--- src/axios-gsm.js
+++ src/axios-gsm.js
@@ -2,11 +2,13 @@ import axios from "axios";
 import Config from "./AppConfig";
 
 const instance = axios.create({
-  baseURL: Config.API_URL
+  baseURL: Config.API_URL,
+  timeout: 600000,
 });
 
 export const axiosOpen = axios.create({
-  baseURL: Config.API_URL
+  baseURL: Config.API_URL,
+  timeout: 600000,
 })
 
 export default instance;
